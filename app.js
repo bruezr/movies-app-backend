f3e330eac31dda7ec2c9de9ba35ddc44b1089b7c
Bruno Rocha <bruezr@icloud.com>
2020-11-12 14:30:24 -0300

    Agrego movies routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const moviesRoutes = require('./routes/movie');
 const app = express();
 
 app.use(express.json());
+
+app.use('/', moviesRoutes);
 
 app.listen(3000);
