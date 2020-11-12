bafc528feb973a3e956eb39cd337c2a61bd6d49c
Bruno Rocha <bruezr@icloud.com>
2020-11-12 14:33:10 -0300

    Agrego controllers de movie

---
+++ controllers/movie.js
@@ -0,0 +1,9 @@
+exports.getMovies = async (req, res, next) => {};
+
+exports.getMovie = async (req, res, next) => {};
+
+exports.createMovie = async (req, res, next) => {};
+
+exports.updateMovie = async (req, res, next) => {};
+
+exports.deleteMovie = async (req, res, next) => {};
