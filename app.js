8f0df37e802f7b51aabcada144f2bb204742ff53
Bruno Rocha <bruezr@icloud.com>
2020-11-12 14:54:04 -0300

    Agrego mongoose db connection

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const moviesRoutes = require('./routes/movie');
+const mongoose = require('mongoose');
 const app = express();
+
+require('dotenv').config({ path: __dirname + '/.env' });
 
 app.use(express.json());
 
 app.use('/', moviesRoutes);
 
-app.listen(3000);
+const databaseUser = process.env.MONGO_USER;
+const databasePsw = process.env.MONGO_PASSWORD;
+const databaseName = process.env.MONGO_DATABASE;
+
+mongoose
+  .connect(
+    `mongodb+srv://${databaseUser}:${databasePsw}@cluster0.gubwk.azure.mongodb.net/${databaseName}`,
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+  )
+  .then(() => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((err) => console.log(err));
