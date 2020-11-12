63df2b1e9e72a075597b5e36292ca66c755c694f
Bruno Rocha <bruezr@icloud.com>
2020-11-12 14:36:47 -0300

    Sumo routes CRUD

--- routes/movie.js
+++ routes/movie.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const movieController = require('../controllers/movie');
 const router = express.Router();
 
-router.get('/');
+router.get('/movies', movieController.getMovies);
+
+router.get('/movie/:movieId', movieController.getMovie);
+
+router.post('/movie', movieController.createMovie);
+
+router.put('/movie/:movieId', movieController.updateMovie);
+
+router.delete('/movie/:movieId', movieController.deleteMovie);
 
 module.exports = router;
