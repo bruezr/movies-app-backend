9504ece4e8b0cd0f3715211ed0429f888e712f4d
Bruno Rocha <bruezr@icloud.com>
2020-11-30 09:05:54 -0300

    Corregí typo

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE'
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Autorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
